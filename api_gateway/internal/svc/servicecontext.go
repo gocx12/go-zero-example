ab4f16fc6970f16edcb3c25f8905eaab3d5df42a
gaochengxi <1825752554@qq.com>
2023-08-13 17:27:07 +0800

    New: finish code generation

---
+++ api_gateway/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"demo/api_gateway/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
